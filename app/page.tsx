578435247bd87f41dea5ef338514d04f6f5b9298
lucastechy <lucastechy1@gmail.com>
2026-02-15 13:12:52 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// app/page.tsx
+export default function Page() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Chatbot is live!</h1>
+      <p>Replace this page with your app later.</p>
+    </div>
+  );
+}
+
